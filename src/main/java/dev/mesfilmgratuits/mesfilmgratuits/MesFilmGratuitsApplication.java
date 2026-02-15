34e1832a684f929e7314ff309b29666cfb85520f
olive <oliverdickens1990@gmail.com>
2026-02-15 12:39:54 +0000

    firstPush
    Signed-off-by: olive <oliverdickens1990@gmail.com>

---
+++ src/main/java/dev/mesfilmgratuits/mesfilmgratuits/MesFilmGratuitsApplication.java
@@ -0,0 +1,13 @@
+package dev.mesfilmgratuits.mesfilmgratuits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesFilmGratuitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesFilmGratuitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mesfilmgratuits/mesfilmgratuits/MesFilmGratuitsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mesfilmgratuits.mesfilmgratuits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesFilmGratuitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
